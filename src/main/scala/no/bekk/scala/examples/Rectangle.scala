c44a624cf9937b1cfd3e4ac5ae5b25e0d06b2c6f
John Ringø <john@ringo.io>
2016-06-10 17:12:43 +0200

    Delete unnecessary files

--- src/main/scala/no/bekk/scala/examples/Rectangle.scala
+++
@@ -1,7 +0,0 @@
-package no.bekk.scala.examples
-
-trait Rectangle {
-	def width: Int
-	def height: Int
-	def surface = width * height
-}
--- src/main/scala/no/bekk/scala/exercises/Ex5_Optionals.scala
+++
@@ -1,9 +0,0 @@
-package no.bekk.scala.exercises
-
-import java.time.LocalDate
-import no.bekk.scala.model.Team
-
-object Ex5_Optionals {
-	
-	def youngestPlayerNameOlderThan(team: Team, minDate: LocalDate): Option[String] = ???
-}
